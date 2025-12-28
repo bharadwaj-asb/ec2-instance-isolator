95ee5e95bdcd03da4359f77dcf7809ccaf7e8c12
bharadwajasb <bharadwajasb@gmail.com>
2025-12-28 22:16:02 +0530

    Added description for each snapshot created

--- lambda_functions/snapshot_volumes/ebs_snapshots.py
+++ lambda_functions/snapshot_volumes/ebs_snapshots.py
@@ -9,28 +9,29 @@ except Exception as e:
 
 # Function to create snapshot and encrypt it
 def main(vols): 
-    try:
         for each_vol in vols:
-            response = client.create_snapshot(
-                VolumeId= each_vol,
-                TagSpecifications=[
-                    {
-                        'Tags': [
-                            {
-                                'Key': 'IncidentStatus',
-                                'Value': 'Quarantined'
-                            },
-                            {
-                                'Key': 'QuarantineTime',
-                                'Value': datetime.now()
-                            }
-                        ]
-                    }
-                ]
-                ) 
+            try:
+                response = client.create_snapshot(
+                    Description= 'This is a quarantined snapshot',
+                    VolumeId= each_vol,
+                    TagSpecifications=[
+                        {
+                            'Tags': [
+                                {
+                                    'Key': 'IncidentStatus',
+                                    'Value': 'Quarantined'
+                                },
+                                {
+                                    'Key': 'QuarantineTime',
+                                    'Value': datetime.now()
+                                }
+                            ]
+                        }
+                    ]
+                    ) 
             # Put each of these snapshots in an S3 bucket
-    except Exception as e:
-        print(f'Exception occurred when deregistering instance: {e}')
+            except Exception as e:
+                print(f'Exception occurred when creating snapshot for volume: {each_vol} : {e}')
 
 
 if __name__ == '__main__':
