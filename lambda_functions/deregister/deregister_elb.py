f150a5d167a5aba2c6053488895c14728531a681
bharadwajasb <bharadwajasb@gmail.com>
2025-12-28 18:33:44 +0530

    Updated working code to deregister_elb.py

--- lambda_functions/deregister/deregister_elb.py
+++ lambda_functions/deregister/deregister_elb.py
@@ -1,25 +1,52 @@
 import boto3
 
-try:
-    client = boto3.client('autoscaling')
-except Exception as e:
-    print(f'Exception occurred when creating auto-scaling client: {e}')
-
-
-def main(instance_id, asg_name):
+# Function to remove from ELB v1
+def elb1(instance_id, elb_name): 
     try:
-        response = client.detach_instances(
-        InstanceIds=[instance_id],
-        AutoScalingGroupName = asg_name,
-        ShouldDecrementDesiredCapacity=False
-        )
-        print(f'Output status is {response['Activities'][0]}') # To be replaced by calling next function in the flow.
-
+        client = boto3.client('elb')
     except Exception as e:
-        print(f'Exception occurred when detaching auto-scaling the the instance: {instance_id} {e}')    
+        print(f'Exception occurred when creating auto-scaling client: {e}')
 
+    try:
+        response = client.deregister_instances_from_load_balancer(
+        LoadBalancerName = elb_name,
+        Instances=[
+            {
+                'InstanceId': instance_id 
+            }
+        ])
+        if 'Instances' in response.keys() and response['Instances']!= []:
+            print(f'Successfully deregistered instance: {instance_id}')
+            return
+        else:
+            print(f'Deregistration failed for instance: {instance_id} from ELB v1')
+    except Exception as e:
+        print(f'Exception occurred when deregistering instance: {e}')
+
+# Function to remove from ELB v2
+def elb2(instance_id, target_group_arn):
+    try:
+        client = boto3.client('elbv2')
+    except Exception as e:
+        print(f'Exception occurred when creating auto-scaling client: {e}')
+
+
+    try:
+        response = client.deregister_targets(
+            TargetGroupArn = target_group_arn,
+            Targets=[
+                {
+                    'Id': instance_id
+                }
+                ]
+            )
+        print(f'Successfully deregistered instance: {instance_id}')
+    except Exception as e:
+        print(f'Exception occurred when deregistering instance: {e}')
 
 if __name__ == '__main__':
     instance_id = ''
-    asg_name = ''
-    main(instance_id, asg_name)
+    elb_name = ''
+    target_group_arn = ''
+    elb1(instance_id, elb_name)
+    elb2(instance_id, target_group_arn)
