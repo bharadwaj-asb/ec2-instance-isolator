f73b5f565c3968500f2a81d88030acb77973f7db
bharadwajasb <bharadwajasb@gmail.com>
2025-12-30 22:06:34 +0530

    Handled InvalidParameterError by adding resource type in tag specification

--- lambda_functions/snapshot_volumes/ebs_snapshots.py
+++ lambda_functions/snapshot_volumes/ebs_snapshots.py
@@ -12,10 +12,11 @@ def main(vols):
         for each_vol in vols:
             try:
                 response = client.create_snapshot(
-                    Description= 'This is a quarantined snapshot',
+                    Description= 'This is a snapshot of a quarantined EBS volume',
                     VolumeId= each_vol,
                     TagSpecifications=[
                         {
+                             'ResourceType': 'snapshot',
                             'Tags': [
                                 {
                                     'Key': 'IncidentStatus',
@@ -23,17 +24,18 @@ def main(vols):
                                 },
                                 {
                                     'Key': 'QuarantineTime',
-                                    'Value': datetime.now()
+                                    'Value': str(datetime.now())
                                 }
                             ]
                         }
                     ]
                     ) 
+                print(response)
             # Put each of these snapshots in an S3 bucket
             except Exception as e:
                 print(f'Exception occurred when creating snapshot for volume: {each_vol} : {e}')
 
 
 if __name__ == '__main__':
-    vols = []
+    vols = ['vol-0ea89318869f3b3e7','vol-0dc1237660cd35cd4']
     main(vols)
