eae4f9df8532bdef19cec71d4af7e2ab4d76c0a3
bharadwajasb <bharadwajasb@gmail.com>
2026-01-05 23:02:21 +0530

    Added logging for success and exception cases

--- lambda_functions/deregister/deregister_elb.py
+++ lambda_functions/deregister/deregister_elb.py
@@ -1,12 +1,27 @@
 import boto3
+import json
+from datetime import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 # Function to remove from ELB v2
-def elb2(instance_id, target_group_arn):
+def elb2(instance_id, target_group_arn,incident_id):
     try:
         client = boto3.client('elbv2')
     except Exception as e:
-        print(f'Exception occurred when creating auto-scaling client: {e}')
+        print(f' {e}')
+        logger.exception({
+        "incident_id": incident_id,
+        "step": "deregister_elb",
+        "instance_id": instance_id,
+        "message":"Exception occurred when creating auto-scaling client",
+        "error": str(e)
+    })
+    #raise
+
 
 
     try:
@@ -18,12 +33,29 @@ def elb2(instance_id, target_group_arn):
                 }
                 ]
             )
-        print(f'Successfully deregistered instance: {instance_id} from ELB v2')
-    except Exception as e:
-        print(f'Exception occurred when deregistering instance from elbv2: {e}')
+        logger.info(
+        {
+            "step": "deregister_elb",
+            "function": "elbv2",
+            "instance_id": instance_id,
+            "incident_id": incident_id,
+            "message": f"Successfully deregistered instance: {instance_id} from ELB v2"
+            })
 
-if __name__ == '__main__':
-    instance_id = 'i-0b2f164b1f525e017'
-    elb_name = 'test-elb-1'
-    target_group_arn = 'arn:aws:elasticloadbalancing:ap-south-1:961341555743:targetgroup/test-tg/cf8a311eeede17b1'
-    elb2(instance_id, target_group_arn)
+    except Exception as e:
+        logger.exception({
+        "incident_id": incident_id,
+        "step": "deregister_elb",
+        "instance_id": instance_id,
+        "message":"Exception occurred when deregistering instance from elbv2:",
+        "error": str(e)
+    }) 
+        #raise
+
+
+#if __name__ == '__main__':
+instance_id = 'i-0b2f164b1f525e017'
+elb_name = 'test-elb-1'
+incident_id = ''
+target_group_arn = 'arn:aws:elasticloadbalancing:ap-south-1:961341555743:targetgroup/test-tg/cf8a311eeede17b1'
+elb2(instance_id, target_group_arn,incident_id)
