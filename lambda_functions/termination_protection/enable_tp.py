ab356e9424c5866074b550e44a20a54b36eb6c61
bharadwajasb <bharadwajasb@gmail.com>
2026-01-05 23:02:58 +0530

    Added logging for success and exception cases

--- lambda_functions/termination_protection/enable_tp.py
+++ lambda_functions/termination_protection/enable_tp.py
@@ -1,21 +1,41 @@
 import boto3
+import json
+from datetime import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 try:
     client = boto3.client('ec2')
 except Exception as e:
-    print(f'Exception occurred when creating EC2 client: {e}')
+    logger.exception({
+        "incident_id": incident_id,
+        "step": "enable_termination_protection",
+        "instance_id": instance_id,
+        "error": str(e)
+    })
+    raise
 
-def main(instance_id):
+def main(instance_id,incident_id):
     try:
         response = client.modify_instance_attribute(InstanceId=instance_id, DisableApiTermination={
         'Value': True
         })
     except Exception as e:
-        print(f'Exception occurred when enabling termination protection for the instance: {instance_id} \n {e}')
+        logger.exception({
+        "incident_id": incident_id,
+        "step": "enable_termination_protection",
+        "instance_id": instance_id,
+        "message": "Error occurred when trying to disable termination protection for the EC2 instance.",
+        "error": str(e)
+    })
+    #raise
 
     
 
 
-if __name__ == '__main__':
-    instance_id = ''
-    main(instance_id)
+#if __name__ == '__main__':
+instance_id = ''
+incident_id = ''
+main(instance_id,incident_id)
