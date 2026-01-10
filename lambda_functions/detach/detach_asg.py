2f4504fdc47641672573bf60326d96a698ddb4ff
bharadwajasb <bharadwajasb@gmail.com>
2026-01-10 21:54:08 +0530

    Updated function to match lambda format

--- lambda_functions/detach/detach_asg.py
+++ lambda_functions/detach/detach_asg.py
@@ -6,20 +6,9 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-try:
-    client = boto3.client('autoscaling')
-except Exception as e:
-    logger.exception({
-                "incident_id": incident_id,
-                "step": "detach_asg",
-                "function": "main",
-                "instance_id": instance_id,
-                "message":"Exception occurred when detaching auto-scaling the the instance:",
-                "error":str(e)
-            })
 
 
-def main(instance_id, asg_name,incident_id):
+def main(client,instance_id, asg_name,incident_id):
     try:
         response = client.detach_instances(
         InstanceIds=[instance_id],
@@ -48,8 +37,25 @@ def main(instance_id, asg_name,incident_id):
  
 
 
-#if __name__ == '__main__':
-instance_id = 'i-0485bd939bfd72eea'
-asg_name = 'test-asg'
-incident_id = ''
-main(instance_id, asg_name, incident_id)
+def lambda_handler(event, context):
+    instance_id = 'i-0485bd939bfd72eea'
+    asg_name = event['ASGNames']
+    incident_id = event['IncidentId']
+    try:
+        client = boto3.client('autoscaling')
+    except Exception as e:
+        logger.exception({
+                "incident_id": incident_id,
+                "step": "detach_asg",
+                "function": "main",
+                "instance_id": instance_id,
+                "message":"Exception occurred when creating auto-scaling client",
+                "error":str(e)
+            })
+
+    main(client,instance_id, asg_name, incident_id)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully detached EC2 instance from autoscaling group:')
+    }
